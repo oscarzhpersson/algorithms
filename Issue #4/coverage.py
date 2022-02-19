43ecb106ae0ae27458f4f75c82dca81d9cf60d49
Oscar Persson <opers@kth.se>
2022-02-19 16:01:29 +0100

    FEAT - Issue #4: Adds flags to each branching statement

---
+++ Issue #4/coverage.py
@@ -0,0 +1,135 @@
+"""
+
+
+   Implements a manual coverage test for  ./intersection.py.
+
+
+   """
+import unittest
+
+
+flags = {
+    "flag0" : False,
+    "flag1-while" : 0,
+    "flag2" : False,
+    "flag3" : False,
+    "flag4" : False,
+    "flag5" : False,
+    "flag6" : False,
+    "flag7" : False,
+    "flag8-while" : 0,
+    "flag9-while" : 0,
+    "flag10" : False,
+    "flag11-else" : False,
+    "flag12" : False
+}
+    
+
+class Node(object):
+    def __init__(self, val=None):
+        self.val = val
+        self.next = None
+
+
+def intersection(h1, h2):
+
+    flags["flag0"] = True
+
+    count = 0
+    flag = None
+    h1_orig = h1
+    h2_orig = h2
+
+    while h1 or h2:
+
+        flags["flags1-while"] += 1 # ? Increment flag!
+
+        count += 1
+
+        if not flag and (h1.next is None or h2.next is None):
+            flags["flag2"] = True # ? Set flag!
+            # We hit the end of one of the lists, set a flag for this
+            flag = (count, h1.next, h2.next)
+
+        if h1:
+            flags["flag4"] = True # ? Set flag!
+            h1 = h1.next
+            
+        if h2:
+            flags["flag5"] = True # ? Set flag!
+            h2 = h2.next
+            
+
+    long_len = count    # Mark the length of the longer of the two lists
+    short_len = flag[0]
+
+    if flag[1] is None:
+        flags["flag6"] = True # ? Set flag!
+        shorter = h1_orig
+        longer = h2_orig
+        
+    elif flag[2] is None:
+        flags["flag7"] = True # ? Set flag!
+        shorter = h2_orig
+        longer = h1_orig
+        
+
+    while longer and shorter:
+
+        flags["flags8-while"] += 1 # ? Increment flag!
+
+        while long_len > short_len:
+
+            flags["flags9-while"] += 1 # ? Increment flag!
+
+            # force the longer of the two lists to "catch up"
+            longer = longer.next
+            long_len -= 1
+
+        if longer == shorter:
+            flags["flag10"] = True # ? Set flag!
+            # The nodes match, return the node
+            return longer
+        else:
+            flags["flag11-else"] = True # ? Set flag!
+            longer = longer.next
+            shorter = shorter.next
+
+    return None
+
+
+class TestSuite(unittest.TestCase):
+
+    def test_intersection(self):
+
+        # create linked list as:
+        # 1 -> 3 -> 5
+        #            \
+        #             7 -> 9 -> 11
+        #            /
+        # 2 -> 4 -> 6
+        a1 = Node(1)
+        b1 = Node(3)
+        c1 = Node(5)
+        d = Node(7)
+        a2 = Node(2)
+        b2 = Node(4)
+        c2 = Node(6)
+        e = Node(9)
+        f = Node(11)
+
+        a1.next = b1
+        b1.next = c1
+        c1.next = d
+        a2.next = b2
+        b2.next = c2
+        c2.next = d
+        d.next = e
+        e.next = f
+
+        self.assertEqual(7, intersection(a1, a2).val)
+
+
+if __name__ == '__main__':
+
+    unittest.main()
