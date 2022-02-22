797df8987941ea624b30f0e9bf972c4e2e0983a6
soderqw <soderqvistw@gmail.com>
2022-02-22 15:37:27 +0100

    FEAT - Issue #5: splits untested if statement into two parts, and changes branch flags accordingly

--- algorithms/matrix/sort_matrix_diagonally.py
+++ algorithms/matrix/sort_matrix_diagonally.py
@@ -23,13 +23,14 @@ from typing import List
 
 flags = {
     "0" : 0, #if
-    "1" : 0, #for
-    "2" : 0, #if
-    "3" : 0, #while
+    "1" : 0, #if
+    "2" : 0, #for
+    "3" : 0, #if
     "4" : 0, #while
-    "5" : 0, #else
-    "6" : 0, #while
+    "5" : 0, #while
+    "6" : 0, #else
     "7" : 0, #while
+    "8" : 0, #while
 }
 
 def sort_diagonally(mat: List[List[int]]) -> List[List[int]]:
@@ -37,17 +38,20 @@ def sort_diagonally(mat: List[List[int]]) -> List[List[int]]:
     # If the input is a vector, return the vector
     # This if statement is never tested, a vector is never given 
     # as input in the unit test for this function
-    if len(mat) == 1 or len(mat[0]) == 1:
+    if len(mat) == 1:
         flags["0"] += 1
         return mat
+    
+    if len(mat[0]) == 1:
+        flags["1"] += 1
 
     # Rows + columns - 1
     # The -1 helps you to not repeat a column
     for i in range(len(mat)+len(mat[0])-1):
-        flags["1"] += 1
+        flags["2"] += 1
         # Process the rows
         if i+1 < len(mat):
-            flags["2"] += 1
+            flags["3"] += 1
             # Initialize heap, set row and column
             h = []
             row = len(mat)-(i+1)
@@ -55,7 +59,7 @@ def sort_diagonally(mat: List[List[int]]) -> List[List[int]]:
 
             # Traverse diagonally, and add the values to the heap
             while row < len(mat):
-                flags["3"] += 1
+                flags["4"] += 1
                 heappush(h, (mat[row][col]))
                 row += 1
                 col += 1
@@ -64,13 +68,13 @@ def sort_diagonally(mat: List[List[int]]) -> List[List[int]]:
             row = len(mat)-(i+1)
             col = 0
             while h:
-                flags["4"] += 1
+                flags["5"] += 1
                 ele = heappop(h)
                 mat[row][col] = ele
                 row += 1
                 col += 1
         else:
-            flags["5"] += 1
+            flags["6"] += 1
             # Process the columns
             # Initialize heap, row and column
             h = []
@@ -79,7 +83,7 @@ def sort_diagonally(mat: List[List[int]]) -> List[List[int]]:
 
             # Traverse Diagonally
             while col < len(mat[0]) and row < len(mat):
-                flags["6"] += 1
+                flags["7"] += 1
                 heappush(h, (mat[row][col]))
                 row += 1
                 col += 1
@@ -88,7 +92,7 @@ def sort_diagonally(mat: List[List[int]]) -> List[List[int]]:
             row = 0
             col = i - (len(mat)-1)
             while h:
-                flags["7"] += 1
+                flags["8"] += 1
                 ele = heappop(h)
                 mat[row][col] = ele
                 row += 1
