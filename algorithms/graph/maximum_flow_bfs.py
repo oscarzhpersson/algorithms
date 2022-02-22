0aeb699e6c15522bcbb533ff14b1d8e5dd3d2c2c
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2022-02-22 16:01:28 +0100

    DOCS - Issue#6: Explains why this function is not possible to refactor due to how its built up

--- algorithms/graph/maximum_flow_bfs.py
+++ algorithms/graph/maximum_flow_bfs.py
@@ -56,7 +56,8 @@ def maximum_flow_bfs(adjacency_matrix):
                     visited[k] = 1
                     bfs.put(k)
                     path[k] = src
-            
+                    
+        #print("Path", path)
         #if there is no path from src to sink
         if(visited[len(new_array) - 1] == 0):
             break
@@ -82,3 +83,98 @@ def maximum_flow_bfs(adjacency_matrix):
         total = total + min
     
     return total
+
+'''
+By doing some tests for this algorithm the conclusion is that this specific algorihm cannot be refactored without large structural change to the entire function
+The reason that this cannot be restructured is just by the way the entire function is set up. At line 33 there is a while loop that runs until there is no path not
+covered and then it breaks from that loop, it ties together and makes the BFS search and maximum flow tied together. By doing so there is no simple way for the function to be refactored into smaller functions which would help the CC of this
+funciton. 
+'''
+
+
+def main():
+    graph = [[0, 16, 13, 0, 0, 0],
+            [0, 0, 10, 12, 0, 0],
+            [0, 4, 0, 0, 14, 0],
+            [0, 0, 9, 0, 0, 20],
+            [0, 0, 0, 7, 0, 4],
+            [0, 0, 0, 0, 0, 0]]
+
+    #print(maximum_flow_bfs(graph))
+
+    graph = [[0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0]]
+
+    print(maximum_flow_bfs(graph))
+   
+'''
+    graph = [[0, 16, 13, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0]]
+
+    total, flags = maximum_flow_bfs(graph)
+    print(flags)
+    print(total)
+
+    graph = [[0, 1, 1, 1, 1, 1],
+             [1, 0, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0, 0]]
+
+    total, flags = maximum_flow_bfs(graph)
+    print(flags)
+    print(total)
+
+'''
+'''
+    graph = [[1000, 1000, 1000, 1000, 1000, 1000],
+             [1000, 1000, 1000, 1000, 1000, 1000],
+             [1000, 1000, 1000, 1000, 1000, 1000],
+             [1000, 1000, 1000, 1000, 1000, 1000],
+             [1000, 1000, 1000, 1000, 1000, 1000],
+             [1000, 1000, 1000, 1000, 1000, 1000]]
+
+    total, flags = maximum_flow_bfs(graph)
+    print(flags)
+    print(total)
+    print("Len of flags: ", len(flags))
+'''
+
+
+'''
+    graph2 = []
+    total, flags2 = maximum_flow_bfs(graph2)
+    print(flags2)
+    print(total2)
+    print("Len of flags: ", len(flags))
+    
+'''
+'''
+    graph3 = [[0, 16, 13, 0, 0, 0],
+            [0, 0, 10, 12, 0, 0],
+            [0, 4, 0, 0, 14, 0],
+            [0, 0, 9, 0, 0, 20]]
+    total3, flags3 = maximum_flow_bfs(graph3)
+    print(flags3)
+    print(total3)
+    print("Len of flags: ", len(flags))
+'''
+'''
+    graph4 = [[0, 16, 13, 0, 0, 0]]
+    total4, flags4 = maximum_flow_bfs(graph4)
+    print(flags4)
+    print(total4)
+    print("Len of flags: ", len(flags))
+'''
+
+if __name__ == "__main__":
+    main()
