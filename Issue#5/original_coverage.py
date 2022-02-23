16d003dd261476a31cf36a10557281818af79ea3
Jansencky <jansencheung0105@gmail.com>
2022-02-23 15:42:48 +0100

    DOCS - Issue #5 add original_coverage to Issue#5 file

---
+++ Issue#5/original_coverage.py
@@ -0,0 +1,27 @@
+from algorithms.bfs import (
+    count_islands,
+    maze_search,
+    shortest_distance_from_all_buildings,
+    ladder_length
+)
+
+import unittest
+
+flag = [0]*6
+
+class TestMazeSearch_Issue4(unittest.TestCase):
+    
+    def test_maze_search(self):
+        grid_1 = [[1,0,1,1,1,1],[1,0,1,0,1,0],[1,0,1,0,1,1],[1,1,1,0,1,1]]
+        self.assertEqual(14, maze_search(grid_1, flag))
+        for i in range(len(flag)):
+            print("Branch ", i, ": ", flag[i])
+        print("\n")
+        grid_2 = [[1,0,0],[0,1,1],[0,1,1]]
+        self.assertEqual(-1, maze_search(grid_2, flag))
+        for i in range(len(flag)):
+            print("Branch ", i, ": ", flag[i])
+        print("\n")
+
+if __name__ == "__main__":
+    unittest.main()
